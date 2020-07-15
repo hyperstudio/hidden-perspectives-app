0e64028aea78733175835f59eef5526c35456b40
Ben Silverman <bsilverm@mit.edu>
2020-07-15 13:31:44 -0400

    #3 add text child to tooltip test

--- src/components/Tooltip/Tooltip.test.js
+++ src/components/Tooltip/Tooltip.test.js
@@ -3,7 +3,11 @@ import { shallow } from 'enzyme';
 import Tooltip from './Tooltip';
 
 describe('Tooltip component', () => {
-	const tooltip = shallow(<Tooltip />);
+	const tooltip = shallow(
+		<Tooltip>
+			Example tooltip text.
+		</Tooltip>,
+	);
 
 	it('should render without crashing', () => {
 		expect(tooltip.exists()).toBe(true);
