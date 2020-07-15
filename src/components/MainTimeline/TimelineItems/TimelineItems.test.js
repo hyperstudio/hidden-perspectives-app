658bf8a0b31b8150a4ad96350be7c9486a481b00
Ben Silverman <bsilverm@mit.edu>
2020-07-15 17:52:17 -0400

    #3 use default props for TimelineItems test

--- src/components/MainTimeline/TimelineItems/TimelineItems.test.js
+++ src/components/MainTimeline/TimelineItems/TimelineItems.test.js
@@ -2,43 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import TimelineItems from './TimelineItems';
 
-const items = [
-	{
-		key: 'woifjewoifjew',
-		year: '2019',
-		months: [
-			{
-				key: 'woiefjweiofjwwgio3',
-				month: 'January',
-				days: [
-					{
-						key: 'qwioucegh283opufq',
-						day: '1',
-						events: [
-							{
-								id: 'wpijfewoipfjwwefwef',
-								title: 'Event',
-								path: '/wiufwef/fbkweu/bfwe',
-								date: new Date(),
-							},
-						],
-						documents: [
-							{
-								id: 'wpijfewoipfjwwefwef',
-								title: 'Event',
-								path: '/wiufwef/fbkweu/bfwfeefwefw',
-								date: new Date(),
-							},
-						],
-					},
-				],
-			},
-		],
-	},
-];
-
 describe('TimelineItems component', () => {
-	const timelineItems = shallow(<TimelineItems timelineItems={items} />);
+	const timelineItems = shallow(<TimelineItems />);
 
 	it('should render without crashing', () => {
 		expect(timelineItems.exists()).toBe(true);
