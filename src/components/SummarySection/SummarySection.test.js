f524f4f3400a06b189941bdb347fbe6050fb2aa5
Ben Silverman <bsilverm@mit.edu>
2020-07-15 13:57:30 -0400

    #3 add SummarySection tests

--- src/components/SummarySection/SummarySection.test.js
+++ src/components/SummarySection/SummarySection.test.js
@@ -2,8 +2,36 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SummarySection from './SummarySection';
 
-describe('SummarySection component', () => {
-	const summarySection = shallow(<SummarySection />);
+describe('SummarySection component with empty tags and empty filteredTags', () => {
+	const summarySection = shallow(<SummarySection
+		tags={[]}
+		filteredTags={[]}
+		history={{ push: jest.fn() }}
+	/>);
+
+	it('should render without crashing', () => {
+		expect(summarySection.exists()).toBe(true);
+	});
+});
+
+describe('SummarySection component with 2 tags and empty filteredTags', () => {
+	const summarySection = shallow(<SummarySection
+		tags={[{ id: 'TestTag1' }, { id: 'TestTag2' }]}
+		filteredTags={[]}
+		history={{ push: jest.fn() }}
+	/>);
+
+	it('should render without crashing', () => {
+		expect(summarySection.exists()).toBe(true);
+	});
+});
+
+describe('SummarySection component with 2 tags and 1 filteredTags', () => {
+	const summarySection = shallow(<SummarySection
+		tags={[{ id: 'TestTag1' }, { id: 'TestTag2' }]}
+		filteredTags={['TestTag1']}
+		history={{ push: jest.fn() }}
+	/>);
 
 	it('should render without crashing', () => {
 		expect(summarySection.exists()).toBe(true);
