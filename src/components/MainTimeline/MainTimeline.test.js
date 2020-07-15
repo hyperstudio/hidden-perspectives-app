86b137559bcb1de1c1f84dee23de2c000c59941e
Ben Silverman <bsilverm@mit.edu>
2020-07-15 16:52:00 -0400

    #3 add required props to MainTimeline test

--- src/components/MainTimeline/MainTimeline.test.js
+++ src/components/MainTimeline/MainTimeline.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import MainTimeline from './MainTimeline';
 
-describe('MainTimeline component', () => {
-	const timeline = shallow(<MainTimeline />);
+describe('MainTimeline component with 0 events, 0 documents, 0 protagonists', () => {
+	const timeline = shallow(<MainTimeline
+		eventsCount={0}
+		documentsCount={0}
+		protagonistsCount={0}
+	/>);
 
 	it('should render without crashing', () => {
 		expect(timeline.exists()).toBe(true);
