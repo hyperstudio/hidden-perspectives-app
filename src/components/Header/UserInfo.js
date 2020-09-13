41486a06a029565776603aeb7d9f644790004e58
Ben Silverman <bsilverm@mit.edu>
2020-09-12 18:19:12 -0600

    #18 Add "New" dropdown to user menu - Migrate UserInfo to recompose - Use Reakit popover, Bootstrap Icons caret-down-fill - Style custom components for popover

--- src/components/Header/UserInfo.js
+++ src/components/Header/UserInfo.js
@@ -1,16 +1,75 @@
 import React from 'react';
+import { withState, lifecycle, compose } from '@hypnosphi/recompose';
+import {
+	usePopoverState,
+	Popover,
+	PopoverDisclosure,
+} from 'reakit/Popover';
 import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
+import { withApollo } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
+import Errors from '../Errors';
+import { withLoading } from '../../utils/hocUtil';
 import { logUserOut, getUserId, isAuthorized } from '../../utils/localStorageUtil';
 import {
-	LogButton, UserInfoContainer, UserAvatar,
+	LogButton,
+	MenuButton,
+	MenuLink,
+	MenuLinkWrapper,
+	PopoverMenu,
+	StyledPopoverArrow,
+	UserAvatar,
+	UserInfoContainer,
 } from './styles';
 
-const UserInfo = ({ userName, adminText }) => (
+const PopoverLink = ({ disclosure, ...props }) => {
+	const popover = usePopoverState({ placement: 'bottom' });
+	return (
+		<>
+			<PopoverDisclosure {...popover} ref={disclosure.ref} {...disclosure.props}>
+				{(disclosureProps) => React.cloneElement(disclosure, disclosureProps)}
+			</PopoverDisclosure>
+			<Popover hideOnClickOutside {...popover} {...props}>
+				<StyledPopoverArrow {...popover} />
+				<PopoverMenu>
+					<MenuLinkWrapper>
+						<MenuLink to="/event/new" as={Link} onClick={popover.hide}>Event</MenuLink>
+					</MenuLinkWrapper>
+					<MenuLinkWrapper>
+						<MenuLink to="/protagonist/new" as={Link} onClick={popover.hide}>Stakeholder</MenuLink>
+					</MenuLinkWrapper>
+				</PopoverMenu>
+			</Popover>
+		</>
+	);
+};
+
+PopoverLink.propTypes = {
+	disclosure: PropTypes.shape().isRequired,
+};
+
+const UserInfo = ({
+	adminText,
+	userName,
+	isLoading,
+	errors,
+}) => !isLoading && (
 	<UserInfoContainer>
-		<UserAvatar>{userName[0].toUpperCase()}</UserAvatar>
+		<Errors errors={errors} />
+		{isAuthorized(['Editor', 'Admin']) && (
+			<>
+				<PopoverLink
+					disclosure={(
+						<MenuButton>
+							New
+							<img src="/icons/caret-down-fill.svg" alt="caret-down-fill" />
+						</MenuButton>
+					)}
+				/>
+			</>
+		)}
+		<UserAvatar>{userName}</UserAvatar>
 		{isAuthorized(['Admin']) && (
 			<LogButton to="/admin" as={Link}>{adminText}</LogButton>
 		)}
@@ -21,12 +80,17 @@ const UserInfo = ({ userName, adminText }) => (
 );
 
 UserInfo.propTypes = {
-	userName: PropTypes.string.isRequired,
+	isLoading: PropTypes.bool,
+	userName: PropTypes.string,
 	adminText: PropTypes.string,
+	errors: PropTypes.arrayOf(PropTypes.string),
 };
 
 UserInfo.defaultProps = {
 	adminText: 'Admin',
+	isLoading: true,
+	userName: '',
+	errors: [],
 };
 
 const USERNAME_QUERY = gql`
@@ -37,17 +101,31 @@ query getUser($id: String) {
 }
 `;
 
-export default () => (
-	<Query query={USERNAME_QUERY} variables={{ id: getUserId() }}>
-		{({ loading, error, data }) => {
-			if (error) {
-				return null;
-			}
-			if (loading) {
-				return null;
-			}
-			return <UserInfo userName={data.user.userName} />;
-		}}
-	</Query>
-);
+const getDataHandler = ({
+	stopLoading,
+	setUserName,
+}) => ({ data }) => {
+	stopLoading();
+	setUserName(data.user.userName[0].toUpperCase());
+};
 
+export default compose(
+	withApollo,
+	withLoading,
+	withState('userName', 'setUserName', ''),
+	withState('errors', 'setErrors', []),
+	lifecycle({
+		componentDidMount() {
+			const { client } = this.props;
+			client.query({
+				query: USERNAME_QUERY,
+				variables: { id: getUserId() },
+			})
+				.then((data) => getDataHandler(this.props)(data))
+				.catch((err) => {
+					this.props.stopLoading();
+					this.props.setErrors([err.message]);
+				});
+		},
+	}),
+)(UserInfo);
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import { PopoverArrow } from 'reakit/Popover';
 
 export const HeaderContainer = styled.header`
 	width: 100%;
@@ -69,6 +70,82 @@ export const LogButton = styled(HeaderLink)`
 	margin-left: 16px;
 `;
 
+export const MenuButton = styled.button`
+	border: none;
+	background: none;
+	color: ${({ theme }) => theme.gray600};
+	font-family: inherit;
+	font-size: 1rem;
+	cursor: pointer;
+	outline: none;
+	text-decoration: none;
+	text-align: left;
+	padding: 0;
+	
+	img {
+		padding-left: 5px;
+	}
+
+	&.active,
+	&:hover,
+	&:focus {
+		color: ${({ theme }) => theme.black};
+	}
+
+	&.active {
+		cursor: default;
+	}
+
+	&:not(.active):hover {
+		text-decoration: none;
+	}
+
+	&:focus {
+		box-shadow: inset 0 0 0 2px ${({ theme }) => theme.gray700};
+	}
+`;
+
+export const PopoverMenu = styled.div`
+	background-color: white;
+	border: 1px solid ${({ theme }) => theme.commonBorderColor};
+	line-height: 1.5rem;
+	padding: 0.5rem 0 0.5rem 0;
+	text-align: left;
+	border-radius: 5%;
+`;
+
+export const MenuLink = styled(HeaderLink)`
+	&:focus {
+		box-shadow: none;
+		border: none;
+	}
+`;
+
+export const MenuLinkWrapper = styled.div`
+	width: 100%;
+	padding-left: 1rem;
+	padding-right: 1rem;
+	padding-top: 0.3rem;
+
+	&:hover {
+		background-color: ${({ theme }) => theme.gray300};
+	}
+`;
+
+export const StyledPopoverArrow = styled(PopoverArrow)`
+	svg {
+		fill: white;
+			transform: translateY(1.5px) !important;
+		path.stroke{
+			stroke: ${({ theme }) => theme.commonBorderColor};
+			stroke-width: 1;
+		}
+		path.fill {
+			stroke :none;
+		}
+	}
+`;
+
 export const UserInfoContainer = styled.div`
 	font-size: 12px;
 	flex-basis: 33%;
@@ -79,6 +156,7 @@ export const UserInfoContainer = styled.div`
 
 export const UserAvatar = styled.span`
 	display: inline-block;
+	margin-left: 16px;
 	width: 1.5rem;
 	height: 1.5rem;
 	box-shadow: 0 0 0 2px ${({ theme }) => theme.gray300};
