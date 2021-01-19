42557aa3a1fbcf65d74320ef4380f00cffc4b7e9
Ben Silverman <bsilverm@mit.edu>
2021-01-19 13:19:53 -0500

    #22 Logout user on token expiry Fixes #22.

--- src/utils/localStorageUtil.js
+++ src/utils/localStorageUtil.js
@@ -1,16 +1,12 @@
 import isDate from 'date-fns/is_date';
 import differenceInMinutes from 'date-fns/difference_in_minutes';
+import { decode } from 'jsonwebtoken';
 import { USER_ROLE, USER_ID, AUTH_TOKEN } from '../state/constants';
 
 export const getUserRole = () => localStorage.getItem(USER_ROLE);
 export const getAuthToken = () => localStorage.getItem(AUTH_TOKEN);
 export const getUserId = () => localStorage.getItem(USER_ID);
 
-export const isAuthenticated = () => Boolean(
-	lib.getUserId() // eslint-disable-line no-use-before-define
-	&& lib.getUserRole() // eslint-disable-line no-use-before-define
-	&& lib.getAuthToken(), // eslint-disable-line no-use-before-define
-);
 
 export const isAuthorized = (authorizedRoles = ['Viewer', 'Editor', 'Admin']) => authorizedRoles
 	.some((role) => role === lib.getUserRole()); // eslint-disable-line no-use-before-define
@@ -19,6 +15,19 @@ export const logUserOut = () => {
 	localStorage.removeItem(AUTH_TOKEN);
 	localStorage.removeItem(USER_ROLE);
 	localStorage.removeItem(USER_ID);
+};
+
+export const isAuthenticated = () => {
+	if (localStorage.getItem(AUTH_TOKEN)) {
+		const decoded = decode(localStorage.getItem(AUTH_TOKEN));
+		const diff = new Date(decoded.exp * 1000) - new Date();
+		if (diff <= 0) { logUserOut(); return false; }
+	}
+	return Boolean(
+		lib.getUserId() // eslint-disable-line no-use-before-define
+    && lib.getUserRole() // eslint-disable-line no-use-before-define
+    && lib.getAuthToken(), // eslint-disable-line no-use-before-define
+	);
 };
 
 export const logUserIn = (token, id, role) => {
